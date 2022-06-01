322fd8affaa05a5f57a0300f621986313d78c0f2
sreytouch <atouch.it@gmail.com>
2022-06-01 00:30:54 -0500

    review

---
+++ js/scopesQuiz.js
@@ -0,0 +1,164 @@
+//Sample Comments
+
+// function main() {//var x;
+//     console.log("x1 is:" + x);//undefined? x is declared but not yet assigned, 
+//     //there is local varaible with same name.
+//     x = 20;
+//     console.log("x2 is:" + x);//20? assign local variable
+//     if (x > 0) {
+//         var x = 30;//
+//         console.log("x3 is:" + x);//30?local variable is reassigned.
+//     }
+//     console.log("x4 is:" + x);//30 (209) var is function scope, so you can read it out of block
+//     var x = 40;
+//     var f = function (x) {
+//         console.log("x5 is:" + x);//50 x is parameter, x is called with 50(217)
+//     };
+//     f(50);
+//     console.log("x6 is:" + x);//40 (213)local variable is assigned 40, 
+// }
+// main();
+// console.log("x7 is:" + x);//10 (213, 209) local declaration of x inside the function.
+
+
+
+// 1. 
+// let grapes = "green"; 
+
+  
+
+// function fruits(){ 
+
+//     return function(message){ 
+
+//         return grapes; 
+
+//     } 
+
+// } 
+
+// console.log(fruits()()); 
+
+
+// 2. 
+// let x = 10; 
+
+// function globalVar(){ 
+
+//     return function(){ 
+
+//        return x; 
+
+//     } 
+
+// } 
+
+// x = 20; 
+
+// let fn = globalVar(); 
+
+// console.log(fn ()); 
+
+
+
+ 
+
+// 3. 
+// function sport(message){ 
+
+//     message = "Hi"; 
+
+//     return function(message){ 
+
+//         return message; 
+
+//     } 
+
+// } 
+
+// let sportFn = sport("Hello"); 
+
+// console.log(sportFn ()); 
+
+
+
+ 
+
+// // 4. 
+// function test(x){ 
+
+//     x = 10; 
+
+//     x++; 
+
+//     return function(){ 
+
+//         return x++; 
+
+//     } 
+
+// } 
+
+// console.log(test(2)()) 
+ 
+ 
+
+// // 5.
+// function car(){ 
+
+//     let color = "white"; 
+
+//     function seat(){ 
+
+//         let color = "brown"; 
+//         console.log(color)//___________
+
+//     } 
+
+//     console.log(color)//___________
+
+// } 
+
+// car("black") 
+
+
+// 6. 
+// function department(){ 
+
+//     let numberOfDepartments = 5; 
+//     console.log(numberOfDepartments);
+//     function software(){ 
+
+//         let language = "javascript"; 
+//         console.log(language)
+
+//     } 
+
+//     console.log(language) 
+
+// } 
+
+// department() 
+
+
+
+ 
+
+// // 7. 
+// function department(language){ 
+
+//     let numberOfDepartments = 5; 
+//     console.log(numberOfDepartments)
+//     function software(){ 
+
+//         let language = "javascript"; 
+//         console.log(language);
+
+//     } 
+
+//     console.log(language) 
+
+// } 
+
+// department("java"); 
+
