d5e146d6e591a49278cd06899e789269182a49cd
sreytouch <atouch.it@gmail.com>
2022-06-01 21:56:25 -0500

    fix some page

--- js/scopesQuiz.js
+++ js/scopesQuiz.js
@@ -1,7 +1,7 @@
 //Sample Comments
 
 // function main() {//var x;
-//     console.log("x1 is:" + x);//undefined? x is declared but not yet assigned, 
+//     console.log("x1 is:" + x);//undefined? x is declared but not yet assigned,
 //     //there is local varaible with same name.
 //     x = 20;
 //     console.log("x2 is:" + x);//20? assign local variable
@@ -15,150 +15,98 @@
 //         console.log("x5 is:" + x);//50 x is parameter, x is called with 50(217)
 //     };
 //     f(50);
-//     console.log("x6 is:" + x);//40 (213)local variable is assigned 40, 
+//     console.log("x6 is:" + x);//40 (213)local variable is assigned 40,
 // }
 // main();
 // console.log("x7 is:" + x);//10 (213, 209) local declaration of x inside the function.
 
+// 1.
+let grapes = "green";
 
+function fruits() {
+    return function (message) {
+        return grapes;
+    };
+}
 
-// 1. 
-// let grapes = "green"; 
+console.log(fruits()()); //prints green : fruits()() calls annonymous function of fruits and "grapes" is defined globally
 
-  
+// 2.
+let x = 10;
 
-// function fruits(){ 
+function globalVar() {
+    return function () {
+        return x;
+    };
+}
 
-//     return function(message){ 
+x = 20;
 
-//         return grapes; 
+let fn = globalVar();
 
-//     } 
+console.log(fn()); //prints 20 : x is set to 20 before calling fn() function
 
-// } 
+// 3.
+function sport(message) {
+    message = "Hi";
+    return function (message) {
+        return message;
+    };
+}
 
-// console.log(fruits()()); 
+let sportFn = sport("Hello");
 
+console.log(sportFn()); //retuns undefined: "message" is parameter for return function, but no parameter is passed
 
-// 2. 
-// let x = 10; 
-
-// function globalVar(){ 
-
-//     return function(){ 
-
-//        return x; 
-
-//     } 
-
-// } 
-
-// x = 20; 
-
-// let fn = globalVar(); 
-
-// console.log(fn ()); 
-
-
-
- 
-
-// 3. 
-// function sport(message){ 
-
-//     message = "Hi"; 
-
-//     return function(message){ 
-
-//         return message; 
-
-//     } 
-
-// } 
-
-// let sportFn = sport("Hello"); 
-
-// console.log(sportFn ()); 
-
-
-
- 
-
-// // 4. 
-// function test(x){ 
-
-//     x = 10; 
-
-//     x++; 
-
-//     return function(){ 
-
-//         return x++; 
-
-//     } 
-
-// } 
-
-// console.log(test(2)()) 
- 
- 
+// // 4.
+function test(x) {
+    x = 10;
+    x++;
+    console.log(x);
+    return function () {
+        return x++;
+    };
+}
+// returns 11 and 11: 2 is passed as parameter but reassigned to 10 as x is local variable, then it is incremented and set to 11,
+// further in return function x is post incremented hence it returns 11  
+console.log(test(2)());
 
 // // 5.
-// function car(){ 
+function car() {
+    let color = "white";
+    function seat() {
+        let color = "brown";
+        console.log(color); //___________
+    }
+    console.log(color); //___________  White
+}
 
-//     let color = "white"; 
+car("black"); // only white will be printed, because seat function is not called.
 
-//     function seat(){ 
+// 6.
+function department() {
+    let numberOfDepartments = 5;
+    console.log(numberOfDepartments); //prints 5
+    function software() {
+        let language = "javascript";
+        console.log(language);
+    }
 
-//         let color = "brown"; 
-//         console.log(color)//___________
+    console.log(language); // language is not defined
+}
 
-//     } 
+department(); //first 5 is printed then undefined is printed as language is not defined
 
-//     console.log(color)//___________
+// // 7.
+function department(language) {
+    let numberOfDepartments = 5;
+    console.log(numberOfDepartments); //prints 5
+    function software() {
+        let language = "javascript";
+        console.log(language);
+    }
 
-// } 
+    console.log(language); //prints "java"
+}
 
-// car("black") 
-
-
-// 6. 
-// function department(){ 
-
-//     let numberOfDepartments = 5; 
-//     console.log(numberOfDepartments);
-//     function software(){ 
-
-//         let language = "javascript"; 
-//         console.log(language)
-
-//     } 
-
-//     console.log(language) 
-
-// } 
-
-// department() 
-
-
-
- 
-
-// // 7. 
-// function department(language){ 
-
-//     let numberOfDepartments = 5; 
-//     console.log(numberOfDepartments)
-//     function software(){ 
-
-//         let language = "javascript"; 
-//         console.log(language);
-
-//     } 
-
-//     console.log(language) 
-
-// } 
-
-// department("java"); 
-
+department("java"); // first 5 is printed as numberOfDepartments is set t0 5 locally and as "java" is sent as parameter in language, it prints "java"
--- js/scopesRules.js
+++ js/scopesRules.js
@@ -1,112 +1,64 @@
-//Sample Comments
-
-// function main() {//var x;
-//     console.log("x1 is:" + x);//undefined? x is declared but not yet assigned,
-//     //there is local varaible with same name.
-//     x = 20;
-//     console.log("x2 is:" + x);//20? assign local variable
-//     if (x > 0) {
-//         var x = 30;//
-//         console.log("x3 is:" + x);//30?local variable is reassigned.
-//     }
-//     console.log("x4 is:" + x);//30 (209) var is function scope, so you can read it out of block
-//     var x = 40;
-//     var f = function (x) {
-//         console.log("x5 is:" + x);//50 x is parameter, x is called with 50(217)
-//     };
-//     f(50);
-//     console.log("x6 is:" + x);//40 (213)local variable is assigned 40,
+//Read value from global scope, reassign the value inside function changes the global scope variable.
+// let x = 1;
+// function a(){
+//     console.log(x);//1
+//     x=2;
+//     console.log(x);//2
 // }
-// main();
-// console.log("x7 is:" + x);//10 (213, 209) local declaration of x inside the function.
+// a();
+// console.log(x);//2 
 
-// 1.
-let grapes = "green";
 
-function fruits() {
-    return function (message) {
-        return grapes;
-    };
+//Object reassigned inside function, doesnt change the outside object
+// let x = {a:1};
+// function a(x){
+//     x={a:4}
+//     console.log(x.a);//4
+// }
+// a(x);
+// console.log(x.a);//1
+
+
+//local variable x is changed, doesnt affect the global variable
+// let x = 1;
+// function a(){
+//     let x=2;
+//     console.log(x);//2
+// }
+// a();
+// console.log(x);//1
+
+
+//parameter x is changed, doesnt affect the global variable
+// let x = 1;
+// function a(x){
+//     x = 4;
+//     console.log(x);//4
+// }
+// a();
+// console.log(x);//1
+
+
+//Declare new variable after changing the global variable, local variable is created, global
+//variable remains unchanged. 
+// let x = 1;
+// function a(){
+//     console.log(x);//undefined
+//     x=2;
+//     console.log(x);//2
+//     var x = 4;
+// }
+// a();
+// console.log(x);//1 
+
+
+//Object pass by reference
+let x = {a:1};
+function a(x){
+    x.a = 4;
+    console.log(x.a);//4
 }
+a(x);
+console.log(x.a);//4
 
-console.log(fruits()()); //prints green : fruits()() calls annonymous function of fruits and "grapes" is defined globally
 
-// 2.
-let x = 10;
-
-function globalVar() {
-    return function () {
-        return x;
-    };
-}
-
-x = 20;
-
-let fn = globalVar();
-
-console.log(fn()); //prints 20 : x is set to 20 before calling fn() function
-
-// 3.
-function sport(message) {
-    message = "Hi";
-    return function (message) {
-        return message;
-    };
-}
-
-let sportFn = sport("Hello");
-
-console.log(sportFn()); //retuns undefined: "message" is parameter for return function, but no parameter is passed
-
-// // 4.
-function test(x) {
-    x = 10;
-    x++;
-    console.log(x);
-    return function () {
-        return x++;
-    };
-}
-// returns 11 and 11: 2 is passed as parameter but reassigned to 10 as x is local variable, then it is incremented and set to 11,
-// further in return function x is post incremented hence it returns 11  
-console.log(test(2)());
-
-// // 5.
-function car() {
-    let color = "white";
-    function seat() {
-        let color = "brown";
-        console.log(color); //___________
-    }
-    console.log(color); //___________  White
-}
-
-car("black"); // only white will be printed, because seat function is not called.
-
-// 6.
-function department() {
-    let numberOfDepartments = 5;
-    console.log(numberOfDepartments); //prints 5
-    function software() {
-        let language = "javascript";
-        console.log(language);
-    }
-
-    console.log(language); // language is not defined
-}
-
-department(); //first 5 is printed then undefined is printed as language is not defined
-
-// // 7.
-function department(language) {
-    let numberOfDepartments = 5;
-    console.log(numberOfDepartments); //prints 5
-    function software() {
-        let language = "javascript";
-        console.log(language);
-    }
-
-    console.log(language); //prints "java"
-}
-
-department("java"); // first 5 is printed as numberOfDepartments is set t0 5 locally and as "java" is sent as parameter in language, it prints "java"
